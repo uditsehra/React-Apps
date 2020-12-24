16ff424e5586ea50376c925532f780836e3afc0b
Udit Sehra <uditsehra@gmail.com>
2020-12-24 22:09:30 +0530

    task list header created

---
+++ task_list/src/components/headerTaskList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeaderTaskList(){
+    return (
+        <div className='taskListHeader'>
+            <h2 style={{flex=1, justifyContent='center'}}>Task List</h2>
+        </div>
+    )
+}
+
+export default HeaderTaskList
